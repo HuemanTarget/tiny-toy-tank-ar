b8a4aea170279cd708922ba5c62b1dd5e56d1c40
Joshua Basche <joshbasche@live.com>
2021-09-12 09:18:26 -0700

    add logic to overlapping animations

--- TinyToyTank/ViewController.swift
+++ TinyToyTank/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
   
   // MARK: - Variables
   var tankAnchor: TinyToyTank._TinyToyTank?
+  var isActionPlaying: Bool = false
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,27 +24,83 @@ class ViewController: UIViewController {
     // Load the "Box" scene from the "Experience" Reality File
     tankAnchor = try! TinyToyTank.load_TinyToyTank()
     
+    // Parent Turret to Tank Body
+    tankAnchor!.cannon?.setParent(tankAnchor!.tank, preservingWorldTransform: true)
+    
+    tankAnchor?.actions.actionComplete.onAction = { _ in
+      self.isActionPlaying = false
+    }
+    
     // Add the box anchor to the scene
     arView.scene.anchors.append(tankAnchor!)
   }
   
+  
   // MARK: - IBActions
   @IBAction func tankRightPressed(_ sender: Any) {
+    if self.isActionPlaying {
+      return
+    } else {
+      self.isActionPlaying = true
+    }
+    
+    tankAnchor!.notifications.tankRight.post()
+    
   }
   
   @IBAction func tankForwardPressed(_ sender: Any) {
+    if self.isActionPlaying {
+      return
+    } else {
+      self.isActionPlaying = true
+    }
+    
+    tankAnchor!.notifications.tankForward.post()
+    
   }
   
   @IBAction func tankLeftPressed(_ sender: Any) {
+    if self.isActionPlaying {
+      return
+    } else {
+      self.isActionPlaying = true
+    }
+    
+    tankAnchor!.notifications.tankLeft.post()
+    
   }
   
   @IBAction func turretRightPressed(_ sender: Any) {
+    if self.isActionPlaying {
+      return
+    } else {
+      self.isActionPlaying = true
+    }
+    
+    tankAnchor!.notifications.turretRight.post()
+    
   }
   
   @IBAction func cannonFirePressed(_ sender: Any) {
+    if self.isActionPlaying {
+      return
+    } else {
+      self.isActionPlaying = true
+    }
+    
+    tankAnchor!.notifications.cannonFire.post()
+    
   }
   
   @IBAction func turretLeftPressed(_ sender: Any) {
+    if self.isActionPlaying {
+      return
+    } else {
+      self.isActionPlaying = true
+    }
+    
+    tankAnchor!.notifications.turretLeft.post()
+    
   }
   
 }
