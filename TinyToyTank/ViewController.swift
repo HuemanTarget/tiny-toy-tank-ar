e497380e13d5ece64f278886b98543a4af3b3416
Joshua Basche <joshbasche@live.com>
2021-09-12 08:34:32 -0700

    add TinyToyTank rcproject to ViewController

--- TinyToyTank/ViewController.swift
+++ TinyToyTank/ViewController.swift
@@ -9,16 +9,20 @@ import UIKit
 import RealityKit
 
 class ViewController: UIViewController {
+  
+  // MARK: - IBOutlets
+  @IBOutlet var arView: ARView!
+  
+  // MARK: - Variables
+  var tankAnchor: TinyToyTank._TinyToyTank?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet var arView: ARView!
+    // Load the "Box" scene from the "Experience" Reality File
+    tankAnchor = try! TinyToyTank.load_TinyToyTank()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-    }
+    // Add the box anchor to the scene
+    arView.scene.anchors.append(tankAnchor!)
+  }
 }
